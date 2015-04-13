f2a5cbff83029c21c6d71c5282a0334ea1984571
Marvin Frommhold <depressiveRobot@users.noreply.github.com>
2015-04-13 14:59:15 +0200

    optimize function that shows stashed changes

--- zsh/show_git_stash.zsh
+++ zsh/show_git_stash.zsh
@@ -1,14 +1,14 @@
 # show a message if there are stashed changes for current branch
 
 show_branch_stash() {
-  if [[ "$(git stash list 2>/dev/null)" != "" ]]
+  branchName=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+  if [[ "$branchName" != "" ]]
   then
-    branchName="On $(git rev-parse --abbrev-ref HEAD 2>/dev/null)"
     if [[ "$(git stash list 2>/dev/null | grep --color=never $branchName)" != "" ]]
     then
       echo
       echored "You have stashed changes:"
-      git stash list 2>/dev/null | grep --color=never "$branchName"
+      git stash list 2>/dev/null | grep -i --color=never "On $branchName"
     fi
   fi
 }
