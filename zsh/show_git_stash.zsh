60a7dab8b682000a489a7a34d309ebe163dbecc1
Marvin Frommhold <depressiveRobot@users.noreply.github.com>
2015-04-14 11:03:57 +0200

    improved wording

--- zsh/show_git_stash.zsh
+++ zsh/show_git_stash.zsh
@@ -7,7 +7,7 @@ show_branch_stash() {
     if [[ "$(git stash list 2>/dev/null | grep --color=never $branchName)" != "" ]]
     then
       echo
-      echored "You have stashed changes:"
+      echoyellow "Stashes for current branch:"
       git stash list 2>/dev/null | grep -i --color=never "On $branchName"
     fi
   fi
