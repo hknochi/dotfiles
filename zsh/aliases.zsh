d17e973646d524a08ef636ca6f5c0964d3730281
Henri Knochenhauer <hknochi@gmail.com>
2015-04-24 11:33:59 +0200

    fix some bugs

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -32,8 +32,8 @@ alias sudo='sudo '
 alias zshconfig="subl ~/.zshrc"
 
 # docker private
-alias d-s1="${PRVT_DOCKER} -H=docker-server1.hknochi.de:2376 "
-alias d-htwk="${PRVT_DOCKER} -H=docker-htwk.hknochi.de:2375 "
+alias d-s1="$PRVT_DOCKER -H=docker-server1.hknochi.de:2376 "
+alias d-htwk="$PRVT_DOCKER -H=docker-htwk.hknochi.de:2375 "
 
 alias ecc-d-i="ecc-docker-includes "
 
@@ -41,11 +41,11 @@ alias ecc-d-i="ecc-docker-includes "
 
 ####ECCENCA
 # docker
-alias d-p1="${ECC_DOCKER} -H=p1.eccenca.com:${ECC_DOCKER_PORT} "
-alias d-p2="${ECC_DOCKER} -H=p2.eccenca.com:${ECC_DOCKER_PORT} "
-alias d-mopsos="${ECC_DOCKER} -H=mopsos.eccenca.com:${ECC_DOCKER_PORT} "
-alias d-idas="${ECC_DOCKER} -H=idas.eccenca.com:${ECC_DOCKER_PORT} "
-alias d-kepheus="${ECC_DOCKER} -H=kepheus.eccenca.com:${ECC_DOCKER_PORT} "
+alias d-p1="$ECC_DOCKER -H=p1.eccenca.com:$ECC_DOCKER_PORT "
+alias d-p2="$ECC_DOCKER -H=p2.eccenca.com:$ECC_DOCKER_PORT "
+alias d-mopsos="$ECC_DOCKER -H=mopsos.eccenca.com:$ECC_DOCKER_PORT "
+alias d-idas="$ECC_DOCKER -H=idas.eccenca.com:$ECC_DOCKER_PORT "
+alias d-kepheus="$ECC_DOCKER -H=kepheus.eccenca.com:$ECC_DOCKER_PORT "
 # get a running instance of our eccenca-base image
 alias ubuntu=runUbuntu
 
