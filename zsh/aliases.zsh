232525b0d1b45155dd62c6d097d74669f960c14c
Henri Knochenhauer <hknochi@gmail.com>
2015-10-29 21:16:11 +0100

    updat dotfiles

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,8 +9,17 @@ alias lr=' tree -d'
 alias cd=' cd'
 alias ..=' cd ..'
 alias ...=' cd ../..'
+alias ....=' cd ../../..'
 
 alias mmv='noglob zmv -W'
+
+# http://rayninfo.co.uk/tips/zshtips.html
+alias -g G='| grep -'
+alias -g P='2>&1 | $PAGER'
+alias -g L='| less'
+alias -g LA='2>&1 | less'
+alias -g M='| most'
+alias -g C='| wc -l'
 
 # fasd
 alias j=' z'
@@ -22,6 +31,8 @@ alias s='subl && subl' # fix empty window if sublime was not running before open
 alias v='vim'
 alias d='docker'
 alias rdf='rdf'
+alias grep='grep --color=auto'
+alias sign='gpg --detach-sign --armor'
 alias updatedb='sudo /usr/libexec/locate.updatedb'
 alias diskusage='ncdu'
 alias rf='rm -rf'
@@ -41,13 +52,91 @@ alias showhiddenfiles="defaults write com.apple.finder AppleShowAllFiles YES"
 # docker private
 export PRVT_DOCKER="docker --tlsverify --tlscacert=$HOME/.config/ssl/pki-hknochi/ca/docker-ca.cer --tlscert=$HOME/.config/ssl/pki-hknochi/certs/docker/client/hknochi.crt --tlskey=$HOME/.config/ssl/pki-hknochi/certs/docker/client/hknochi-rsa.key "
 
-alias d-s1="$PRVT_DOCKER -H=docker-server1.hknochi.de:2376 "
+alias d-s1="$PRVT_DOCKER -H=docker-server1.hknochi.de:2375 "
 alias d-htwk="$PRVT_DOCKER -H=docker-htwk.hknochi.de:2375 "
-
+alias dm="docker-machine"
 alias private="$HOME/Documents/encfslogin.sh"
 
 # file suffix aliases
 alias -s pdf="o"
 alias -s md="open -a /Applications/Marked\ 2.app"
 alias -s {png,PNG,jpg,jpeg,JPG,JPEG}="o"
+alias -s jar="java -jar"
+alias -s deb="sudo dpkg -i"
+alias -s gpg="gpg"
+alias -s iso="vlc"
+alias -s avi=" open"
+alias -s AVI=" open"
+alias -s mov=" open"
+alias -s mpg=" open"
+alias -s m4v=" open"
+alias -s mp4=" open"
+alias -s rmvb=" open"
+alias -s MP4=" open"
+alias -s ogg=" open"
+alias -s ogv=" open"
+alias -s flv=" open"
+alias -s mkv=" open"
+alias -s wav=" open"
+alias -s mp3=" open"
+alias -s webm=" open"
 
+alias -s tif="open"
+alias -s tiff="open"
+alias -s png="open"
+alias -s jpg="open"
+alias -s jpeg="open"
+alias -s JPG="open"
+alias -s gif="open"
+alias -s svg="open"
+alias -s psd="open"
+
+alias -s com="open"
+alias -s de="open"
+alias -s org="open"
+
+alias -s rdf="rapper --count"
+alias -s owl="rapper --count"
+alias -s ttl="rapper -i turtle --count"
+alias -s tt="rapper -i turtle --count"
+alias -s n3="rapper -i turtle --count"
+alias -s nq="rapper -i nquads --count"
+alias -s nt="rapper -i ntriples --count"
+alias -s ntriples="rapper -i ntriples --count"
+alias -s ntriple="rapper -i ntriples --count"
+alias -s trig="rapper -i trig --count"
+
+alias -s ods="open"
+alias -s xls="open"
+alias -s xlsx="open"
+alias -s csv="open"
+
+alias -s pot="open"
+alias -s odt="open"
+alias -s doc="open"
+alias -s docx="open"
+alias -s rtf="open"
+alias -s dot="dot -Tpng -O"
+
+alias -s ppt="open"
+alias -s pptx="open"
+alias -s odp="open"
+
+alias -s plist="plutil"
+alias -s log="open"
+
+alias -s sla="open"
+
+alias -s exe="open"
+
+alias -s tjp="tj3"
+alias -s asc="gpg"
+alias -s pem="openssl x509 -noout -text -in "
+
+alias -s zip="extract $@"
+alias -s rar="extract $@"
+alias -s tar.gz="extract $@"
+
+# fancy mac aliases
+alias pw="pwgen -Cs 20 1 | tr -d ' ' | tr -d '\n' | pbcopy"
+alias macfind="mdfind -name '$@'"
